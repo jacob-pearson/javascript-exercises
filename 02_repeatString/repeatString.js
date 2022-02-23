d8b70643486a09c80c0255ecc2caa0ab1f67c943
jacob-pearson <jcbpearson09@gmail.com>
2022-02-23 11:58:22 -0600

    solves exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    const stringArray = [];
+    let returnString = '';
+    if(num < 0){
+        returnString = 'ERROR';
+    }else{
+        let i = 0;
+        while(i < num){
+            stringArray.push(string);
+            i++;
+        }
+        returnString = stringArray.join('');
+    }
 
+    return returnString;
+
+    
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
