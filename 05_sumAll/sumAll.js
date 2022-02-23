6ec2152c1d22ed9df05e20638d876bb60626d410
jacob-pearson <jcbpearson09@gmail.com>
2022-02-23 17:37:17 -0600

    Solves exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
+    let finalSum;
+    let difference;
+    let large;
+    if((Number.isFinite(int1) && int1 > 0) && (Number.isFinite(int2) && int2 > 0)){
+        if(int1 > int2){
+            difference = int1 - int2;
+            large = int2;
+            finalSum = int2;
+        }else{
+            difference = int2 - int1;
+            large = int1;
+            finalSum =int1;
+        }
+    
+        for(i = large; i <= difference; i++){
+            nextInt = i + 1;
+            finalSum += nextInt;
+    
+        }
+    
+        return finalSum;
+    }else{
+        return 'ERROR';
+    }
+
 
 };
 
