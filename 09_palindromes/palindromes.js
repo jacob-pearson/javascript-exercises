e964134680b9a0fb641c27463c5f0efc263070a2
jacob-pearson <jcbpearson09@gmail.com>
2022-06-09 18:38:44 -0500

    finishes exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    s = s.replace(/ /g, '')
+    s = s.replace('.','')
+    s = s.replace('!','')
+    s = s.replace(/,/g,'')
+    s = s.replace('?','')
+    s = s.toUpperCase()
+
+    let stringArray = s.split('')
+    let testArray = s.split('').reverse()
+    return Array.isArray(stringArray) && Array.isArray(testArray) &&
+    stringArray.length === testArray.length &&
+    stringArray.every((val, index) => val === testArray[index])
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
