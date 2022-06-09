c5ba0157299fcf6c85d726ac0d9b0e9f4c427c2c
jacob-pearson <jcbpearson09@gmail.com>
2022-06-09 17:02:37 -0500

    finalizes this exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,50 @@
-const add = function() {
+const add = function(a, b) {
+  let sum = a + b
+  return sum
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  let result = a - b
+  return result
 	
 };
 
-const sum = function() {
+const sum = function(a) {
+  let total = 0
+  for(let i = 0; i < a.length; i++){
+    total += a[i]
+  }
+  return total
 	
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let total = 1
+  for(let i = 0; i < a.length; i++){
+    total = total * a[i]
+  }
+  return total
+
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a ** b
+  return result
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  let numbers = []
+  let total = 1
+  while(a > 0){
+    numbers.push(a)
+    a--
+  }
+  for(let i = 0; i < numbers.length; i++){
+    total = total * numbers[i]
+  }
+  return total
 	
 };
 
